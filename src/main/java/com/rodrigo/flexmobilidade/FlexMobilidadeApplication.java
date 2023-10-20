89c874d9e69bad58ee14244a1e189913b6d3d2c2
Rodrigodx <rodrigodx52@gmail.com>
2023-10-20 15:25:47 -0300

    First commit

---
+++ src/main/java/com/rodrigo/flexmobilidade/FlexMobilidadeApplication.java
@@ -0,0 +1,13 @@
+package com.rodrigo.flexmobilidade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlexMobilidadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlexMobilidadeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rodrigo/flexmobilidade/FlexMobilidadeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rodrigo.flexmobilidade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlexMobilidadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
