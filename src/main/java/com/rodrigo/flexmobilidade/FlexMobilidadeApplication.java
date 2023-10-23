54e9abe54e4f75612a95c215d6be7aa04aff475d
Rodrigodx <rodrigodx52@gmail.com>
2023-10-23 15:28:30 -0300

    Added Swagger

--- src/main/java/com/rodrigo/flexmobilidade/FlexMobilidadeApplication.java
+++ src/main/java/com/rodrigo/flexmobilidade/FlexMobilidadeApplication.java
@@ -1,9 +1,12 @@
 package com.rodrigo.flexmobilidade;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "Flex Mobilidade", version = "1", description = "API desenvolvida para locação de veículos"))
 public class FlexMobilidadeApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/rodrigo/flexmobilidade/controllers/GrupoController.java
+++ src/main/java/com/rodrigo/flexmobilidade/controllers/GrupoController.java
@@ -2,6 +2,7 @@ package com.rodrigo.flexmobilidade.controllers;
 
 import com.rodrigo.flexmobilidade.model.Grupo;
 import com.rodrigo.flexmobilidade.services.GrupoService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,12 +11,13 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/api/grupo")
+@RequestMapping(value = "/api/grupo", produces = {"application/json"} )
 @RequiredArgsConstructor
 public class GrupoController {
 
     private final GrupoService grupoService;
 
+    @Operation(summary = "Salva um Categoria de carros", method = "POST")
     @PostMapping
     public ResponseEntity<Grupo> save (@RequestBody Grupo grupo){
         return new ResponseEntity<>(grupoService.save(grupo), HttpStatus.CREATED);
