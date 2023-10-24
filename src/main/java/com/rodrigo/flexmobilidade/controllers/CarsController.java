5171ada1f3e2798dbc54851262ed318df9f6ec18
Rodrigodx <rodrigodx52@gmail.com>
2023-10-24 11:49:53 -0300

    Create CarsController with two end points(Save and FindAll)

---
+++ src/main/java/com/rodrigo/flexmobilidade/controllers/CarsController.java
@@ -0,0 +1,34 @@
+package com.rodrigo.flexmobilidade.controllers;
+
+import com.rodrigo.flexmobilidade.model.Cars;
+import com.rodrigo.flexmobilidade.services.CarsService;
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/api/cars", produces = {"application/json"})
+@RequiredArgsConstructor
+public class CarsController {
+
+    private final CarsService carsService;
+
+    @Operation(summary = "Realiza cadastro de carros no sistema", method = "POST")
+    @PostMapping
+    public ResponseEntity<Cars> save(Cars cars){
+        return new ResponseEntity<>(carsService.save(cars), HttpStatus.CREATED);
+    }
+
+    @Operation(summary = "Mostra todos os carros cadastrados", method = "GET")
+    @GetMapping
+    public ResponseEntity<List<Cars>> findAll(){
+        return ResponseEntity.ok(carsService.findAll());
+    }
+}
