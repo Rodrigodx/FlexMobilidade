b4bfdf729fa05cd39b833c9dbfe39886194d114f
Rodrigodx <rodrigodx52@gmail.com>
2023-10-26 13:30:53 -0300

    Changed the class Grupo to an Embeddable class

--- src/main/java/com/rodrigo/flexmobilidade/repositories/GrupoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.rodrigo.flexmobilidade.repositories;
-
-import com.rodrigo.flexmobilidade.model.Grupo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface GrupoRepository extends JpaRepository<Grupo, Integer> {
-}
