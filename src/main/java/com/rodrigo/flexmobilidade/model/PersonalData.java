7b9be89344e5ea9f277599e5c2bfdf95f14cc02b
Rodrigodx <rodrigodx52@gmail.com>
2023-10-29 12:20:35 -0300

    Created class PersonalData

---
+++ src/main/java/com/rodrigo/flexmobilidade/model/PersonalData.java
@@ -0,0 +1,18 @@
+package com.rodrigo.flexmobilidade.model;
+
+import jakarta.persistence.Embeddable;
+import lombok.Data;
+
+@Embeddable
+@Data
+public class PersonalData {
+
+    private String name;
+
+    private String cpf;
+
+    private String email;
+
+    private String telefone;
+
+}
--- src/main/java/com/rodrigo/flexmobilidade/model/Reserva.java
+++ src/main/java/com/rodrigo/flexmobilidade/model/Reserva.java
@@ -1,9 +1,6 @@
 package com.rodrigo.flexmobilidade.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -28,5 +25,7 @@ public class Reserva {
 
     private LocalDateTime finish;
 
+    @Embedded
+    private PersonalData personalData;
 
 }
