5536d2159f8fc26893ef823926a141201b02c78b
Rodrigodx <rodrigodx52@gmail.com>
2023-11-01 14:51:49 -0300

    Created class AdditionalUtility and AdditionalUtilityRepository

---
+++ src/main/java/com/rodrigo/flexmobilidade/model/additionalutility/AdditionalUtility.java
@@ -0,0 +1,27 @@
+package com.rodrigo.flexmobilidade.model.additionalutility;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AdditionalUtility {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String name;
+
+    private Double value;
+
+    private Integer quant = 0;
+
+}
---
+++ src/main/java/com/rodrigo/flexmobilidade/repositories/AdditionalUtilityRepository.java
@@ -0,0 +1,7 @@
+package com.rodrigo.flexmobilidade.repositories;
+
+import com.rodrigo.flexmobilidade.model.additionalutility.AdditionalUtility;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AdditionalUtilityRepository extends JpaRepository<AdditionalUtility, Integer> {
+}
