f3f67307d2ef4125bed2233d8a61acc517fa4353
Rodrigodx <rodrigodx52@gmail.com>
2023-11-07 14:35:23 -0300

    Created two more DTOs

---
+++ src/main/java/com/rodrigo/flexmobilidade/model/accessories/dto/AccessoryDTO.java
@@ -0,0 +1,10 @@
+package com.rodrigo.flexmobilidade.model.accessories.dto;
+
+import lombok.Data;
+
+@Data
+public class AccessoryDTO {
+
+    private Integer id;
+
+}
---
+++ src/main/java/com/rodrigo/flexmobilidade/model/additionalutility/dto/AdditionalUtilityDTO.java
@@ -0,0 +1,10 @@
+package com.rodrigo.flexmobilidade.model.additionalutility.dto;
+
+import lombok.Data;
+
+@Data
+public class AdditionalUtilityDTO {
+
+    private Integer id;
+
+}
