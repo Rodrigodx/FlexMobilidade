31425fc38518926a4ffc858f63e33e1761ebbe40
Rodrigodx <rodrigodx52@gmail.com>
2023-11-07 14:38:17 -0300

    changed the class of the List attributes.

--- src/main/java/com/rodrigo/flexmobilidade/model/reserva/dto/ReservaRequestDTO.java
+++ src/main/java/com/rodrigo/flexmobilidade/model/reserva/dto/ReservaRequestDTO.java
@@ -1,12 +1,15 @@
 package com.rodrigo.flexmobilidade.model.reserva.dto;
 
 import com.rodrigo.flexmobilidade.model.accessories.Accessory;
+import com.rodrigo.flexmobilidade.model.accessories.dto.AccessoryDTO;
 import com.rodrigo.flexmobilidade.model.additionalutility.AdditionalUtility;
+import com.rodrigo.flexmobilidade.model.additionalutility.dto.AdditionalUtilityDTO;
 import com.rodrigo.flexmobilidade.model.protections.Protection;
 import com.rodrigo.flexmobilidade.model.reserva.PersonalData;
 import lombok.Data;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 @Data
@@ -21,9 +24,10 @@ public class ReservaRequestDTO {
 
     private PersonalData personalData;
 
-    private Protection protection;
+    private Integer idProtection;
 
-    private List<Accessory> accessory;
+    private List<AccessoryDTO> accessories = new ArrayList<>();
 
-    private List<AdditionalUtility> additionalUtility;
+    private List<AdditionalUtilityDTO> additionalUtilities = new ArrayList<>();
+
 }
