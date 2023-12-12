47fdb57936a6f9d0dc3841e76e582d527d169d7d
Rodrigodx <rodrigodx52@gmail.com>
2023-12-12 15:22:44 -0300

    Added CommandLineRunner

--- src/main/java/com/rodrigo/flexmobilidade/FlexMobilidadeApplication.java
+++ src/main/java/com/rodrigo/flexmobilidade/FlexMobilidadeApplication.java
@@ -1,16 +1,37 @@
 package com.rodrigo.flexmobilidade;
 
+import com.rodrigo.flexmobilidade.model.user.User;
+import com.rodrigo.flexmobilidade.model.user.UserRole;
+import com.rodrigo.flexmobilidade.repositories.UserRepository;
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 @OpenAPIDefinition(info = @Info(title = "Flex Mobilidade", version = "1", description = "API desenvolvida para locação de veículos"))
-public class FlexMobilidadeApplication {
+public class FlexMobilidadeApplication implements CommandLineRunner {
+
+
+	@Autowired
+	private UserRepository userRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(FlexMobilidadeApplication.class, args);
 	}
 
+	public void run(String... args) throws Exception {
+		User adminAccount = userRepository.findByRole(UserRole.ADMIN);
+		if (null == adminAccount) {
+			User user = new User();
+			user.setEmail("admin@gmail.com");
+			user.setName("rodrigo");
+			user.setRole(UserRole.ADMIN);
+			user.setPassword(new BCryptPasswordEncoder().encode("admin"));
+			userRepository.save(user);
+		}
+	}
 }
