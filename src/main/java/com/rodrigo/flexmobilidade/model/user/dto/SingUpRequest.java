c9de0622eb5af4dfcf38007fda4ca2feb60cc011
Rodrigodx <rodrigodx52@gmail.com>
2023-12-12 15:19:11 -0300

    Created AuthenticationController with method 'singup'

---
+++ src/main/java/com/rodrigo/flexmobilidade/model/user/dto/SingUpRequest.java
@@ -0,0 +1,15 @@
+package com.rodrigo.flexmobilidade.model.user.dto;
+
+import lombok.Data;
+
+@Data
+public class SingUpRequest {
+
+    private String name;
+
+    private String email;
+
+    private String password;
+
+
+}
--- src/main/java/com/rodrigo/flexmobilidade/repositories/RoleRepository.java
+++
@@ -1,7 +0,0 @@
-package com.rodrigo.flexmobilidade.repositories;
-
-import com.rodrigo.flexmobilidade.model.role.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role, Integer> {
-}
