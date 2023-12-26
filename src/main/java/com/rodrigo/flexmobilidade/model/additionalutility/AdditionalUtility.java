1fe87e27ffe23815564dfeb5862ef4638ccdb3c1
Rodrigodx <rodrigodx52@gmail.com>
2023-12-26 11:10:56 -0300

    Added annotation @Column in additionalUtility class and protection class

--- src/main/java/com/rodrigo/flexmobilidade/model/additionalutility/AdditionalUtility.java
+++ src/main/java/com/rodrigo/flexmobilidade/model/additionalutility/AdditionalUtility.java
@@ -1,9 +1,6 @@
 package com.rodrigo.flexmobilidade.model.additionalutility;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -20,6 +17,7 @@ public class AdditionalUtility {
 
     private String name;
 
+    @Column(name = "value_column")
     private Double value;
 
     private Integer quantity = 0;
--- src/main/java/com/rodrigo/flexmobilidade/model/protections/Protection.java
+++ src/main/java/com/rodrigo/flexmobilidade/model/protections/Protection.java
@@ -1,9 +1,6 @@
 package com.rodrigo.flexmobilidade.model.protections;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -22,6 +19,7 @@ public class Protection {
 
     private String benefits;
 
+    @Column(name = "value_column")
     private Double value;
 
 
