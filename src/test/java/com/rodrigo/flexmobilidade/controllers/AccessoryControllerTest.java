5512d0a70c6846de888c234afcc07f4192cc8dc3
Rodrigodx <rodrigodx52@gmail.com>
2024-01-06 15:59:36 -0300

    Added test for endpoint FindById in AccessoryController class

---
+++ src/test/java/com/rodrigo/flexmobilidade/controllers/AccessoryControllerTest.java
@@ -0,0 +1,77 @@
+package com.rodrigo.flexmobilidade.controllers;
+
+import com.rodrigo.flexmobilidade.dto.accessories.AccessoryRequestDTO;
+import com.rodrigo.flexmobilidade.dto.accessories.AccessoryResponseDTO;
+import com.rodrigo.flexmobilidade.model.accessories.Accessory;
+import com.rodrigo.flexmobilidade.services.AccessoryService;
+import org.apache.coyote.Response;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.http.ResponseEntity;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.when;
+
+class AccessoryControllerTest {
+    public static final int ID = 1;
+    public static final String NAME = "Test";
+    public static final double VALUES = 25.00;
+    public static final int INDEX = 0;
+    private AccessoryRequestDTO accessoryRequestDTO;
+    private AccessoryResponseDTO accessoryResponseDTO;
+    private Accessory accessory;
+
+    @InjectMocks
+    private AccessoryController accessoryController;
+
+    @Mock
+    private AccessoryService accessoryService;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        startAccessory();
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void whenFindByIdThenReturnSuccess() {
+        when(accessoryService.findById(eq(ID))).thenReturn(accessory);
+
+        ResponseEntity<Accessory> response = accessoryController.findById(ID);
+
+        assertNotNull(response);
+        assertNotNull(response.getBody());
+        assertEquals(ResponseEntity.class, response.getClass());
+
+        assertEquals(ID, response.getBody().getId());
+        assertEquals(NAME, response.getBody().getName());
+        assertEquals(VALUES, response.getBody().getValues());
+
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void update() {
+    }
+
+    private void startAccessory(){
+        accessory = new Accessory(ID, NAME, VALUES);
+        accessoryRequestDTO = new AccessoryRequestDTO(NAME, VALUES);
+        accessoryResponseDTO = new AccessoryResponseDTO(ID, NAME, VALUES);
+    }
+}
